9b2200938218d197efeeb53f12627f6e2c10ea20
ruhiel_murrue <ruhiel1983@gmail.com>
2016-07-30 08:18:54 +0900

    バージョン番号変更

--- OrganizationMemoPlugin/OrganizationMemoPlugin.cs
+++ OrganizationMemoPlugin/OrganizationMemoPlugin.cs
@@ -7,7 +7,7 @@ namespace OrganizationMemoPlugin
     [ExportMetadata("Guid", "7100062E-9D96-40F6-A06B-4DAA850B472C")]
     [ExportMetadata("Title", "OrganizationMemo")]
     [ExportMetadata("Description", "艦隊編成メモ一覧を表示します。")]
-    [ExportMetadata("Version", "0.0.1")]
+    [ExportMetadata("Version", "0.0.2")]
     [ExportMetadata("Author", "@ruhiel_murrue")]
     [Export(typeof(ITool))]
     public class OrganizationMemoPlugin : ITool, IPlugin
